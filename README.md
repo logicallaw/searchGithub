17f3ead6a7c1f35f997e8a74fdcaeaa38337cb8f
logicallaw <logicallawbio@gmail.com>
2024-01-27 21:28:28 +0900

    update:description

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# searchGithub
-search the code of Github for Tistory
+searchGithub
+=============
+search the Github code!
+You can crawl your code by Puppeteer.
+-------------
+This is a normal paragraph:
+
+    const link = "https://github.com/[the_name_of_Github]/[your_repository]/[your_code_file]"
+    
+end code block.
--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const puppeteer = require("puppeteer");
         height: 1080
     });
 
-    //크롤링을 위해 페이지 이동합니다.
-    //https://github.com/[github_name]/[repository_name]/[code_file_name]
+    //Let's go to the page for crawling.
+    //example : "https://github.com/[the_name_of_Github]/[your_repository]/[your_code_file]"
     const link = "https://github.com/logicallaw/baekjoon/blob/main/baekjoon/10810-2.cpp";
     await page.goto(link);
 
-    //selector 변수에 copy된 selector 주소를 붙여놓습니다.
+    //Attach the copied selector address to the selector variable.
     const selector = '#read-only-cursor-text-area';
     await page.waitForSelector(selector);
-    //selector의 텍스트를 읽어 code 변수에 저장합니다.
+    //Reads the text of the selector and saves it in the code variable.
     const code = await page.$eval(selector, (element) => element.textContent)
 
-    console.log('크롤링한 코드를 출력합니다.\n', code);
+    console.log('print the code variable\n', code);
     await broswer.close();
 })();
